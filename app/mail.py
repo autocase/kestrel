3f7de11fcbb979dce93c1c949bb84281dde578cf
Dillon <nixhatter@gmail.com>
2019-06-11 17:34:25 -0400

    sending email using transaction template

--- app/mail.py
+++ app/mail.py
@@ -15,8 +15,14 @@ def send_email_to(recipient):
         to_emails='to@example.com',
         subject='Sending with Twilio SendGrid is Fun',
         html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    message.dynamic_template_data = {
+        'user_name': 'A NEW NAME'
+    }
+
+    message.template_id = 'd-3365a1e105b746cfaf6e95c8ce944b64'
+
     try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(env_sendgrid_key)
         response = sg.send(message)
         print(response.status_code)
         print(response.body)
@@ -24,4 +30,6 @@ def send_email_to(recipient):
     except Exception as e:
         logger.error("A sendgrid error occurred: {} - {}".format(e.__class__, e.__str__()))
         raise e
+
+
 
